d94c94c422d215609c15af0b6967ac43d414615a
Dan Rose <dan@digilabs.io>
2019-09-25 15:13:09 -0500

    Added SL2 parsing. Not complete and the construct library is sort of terrible.

--- navico.py
+++ navico.py
@@ -1,123 +1,116 @@
 import enum
 import struct
 from dataclasses import dataclass
+from pathlib import Path
+from pprint import pprint
 
+import construct as c
 
-@dataclass
-class FileHeader:
-    version: int
-    hardware_version: int
-    block_size: int
+sl_file_header = c.Struct(
+    version=c.Enum(c.Int16ul, slg=1, sl2=2, sl3=3),
+    hardware_version=c.Int16ul,
+    block_size=c.Int16ul,
+    p_=c.Padding(1)
+)
 
-    def to_bytes(self):
-        fmt_header = struct.Struct('<3H')
-        buffer = bytearray(self.blocksize)
-        fmt_header.pack_into(buffer, 0, self.version, self.hardware_version, self.block_size)
-        return buffer
+SL2_HEADER = sl_file_header.build(dict(
+    version=2,
+    hardware_version=1,
+    block_size=3200,  # sometimes 1970 sometimes 3200
+))
 
+SL3_HEADER = sl_file_header.build(dict(
+    version=3,
+    hardware_version=1,
+    block_size=3200,
+))
+test = c.Struct(
+    a=c.Int8ul,
+    b=c.Padding(4),
+    c=c.Int8ul
+)
 
-SL2_HEADER = FileHeader(version=2, hardware_version=1, block_size=1970)
+sl2_frame = c.Struct(
+    # 0
+    frame_offset=c.Int32ul,
+    last_frame_offset_primary=c.Int32ul,
+    last_frame_offset_secondary=c.Int32ul,
+    last_frame_offset_down_scan=c.Int32ul,
+    last_frame_offset_left=c.Int32ul,
+    last_frame_offset_right=c.Int32ul,
+    last_frame_offset_composite=c.Int32ul,
+    # 28
+    frame_size=c.Int16ul,
+    previous_frame_size=c.Int16ul,
+    channel_type=c.Enum(
+        c.Int16ul, Primary=0, Secondary=1, DownScan=2,
+        SidescanLeft=3, SidescanRight=4, SidescanComposite=5),
+    packet_size=c.Int16ul,
+    # 36
+    frame_index=c.Int32ul,
+    upper_limit_feet=c.Float32l,
+    lower_limit_feet=c.Float32l,
+    # 48
+    # pad48=c.Bytes(50-48),
+    # # 50
+    # frequency=c.Enum(c.Int8ul, KHz200=0, KHz50=1, KHz83=2, KHz455=3, KHz800=4,
+    # #     Khz38=5, KHz28=6, Khz130Khz210=7, Khz90Khz150=8, KHz40Khz60=9,
+    # #     KHz25KHz45=10),
+    pad51=c.Bytes(60 - 48),
+    # 60
+    time1=c.Int32ul,  # current unix epoch = 1569437027
+    water_depth_feet=c.Float32l,
+    keel_depth_feet=c.Float32l,
+    # 68
+    pad2=c.Bytes(100 - 72),
+    # 100
+    speed_knots=c.Float32l,
+    water_temperature_c=c.Float32l,
+    easting=c.Int32ul,
+    northing=c.Int32ul,
+    water_speed_knots=c.Float32l,
+    course_over_ground_radians=c.Float32l,
+    altitude_ft=c.Float32l,
+    heading_radians=c.Float32l,
+    # 132
+    flags=c.FlagsEnum(
+        c.Int16ul,
+        TrackValid=0x80,
+        WaterSpeedValid=0x40,
+        PositionValid=0x10,
+        WaterTempValid=0x04,
+        SpeedValid=0x02,
+        AltitudeValid=0x200,
+        HeadingValid=0x100,
+    ),
+    # 134
+    _pad3=c.Bytes(140 - 134),
+    # 140
+    time_offset=c.Int32ul,
+    sounded_data=c.Array(c.this.packet_size,c.Int8ul),
+    # sounded_data=c.Array(get_block_size, c.Int8ul)
+    #
+)
 
+sl2_file = c.Struct(
+    header=sl_file_header,
+    blocks=c.Array(10, c.FixedSized(2064, sl2_frame))
+)
+if __name__ == '__main__':
+    file_bytes = Path('input.sl2').read_bytes()
+    # for i in range(len(file_bytes)):
+    #     if c.Int32ul.parse(file_bytes[i:]) == i:
+    #         print(i)
 
-class ValidityFlag(enum.IntFlag):
-    Track = 0
-    Position = 1 << 3
-    CourseOrSpeed = 1 << 5
-    Speed = 1 << 6
-    AltitudeOrCourseOrSpeed = 1 << 9
-    Altitude = 1 << 14
-    Heading = 1 << 15
-
-
-@dataclass
-class SL2FrameHeader:
-    frame_offset = 0
-    last_frame_offset_primary = 0
-    last_frame_offset_secondary = 0
-    last_frame_offset_down_scan = 0
-    last_frame_offset_left = 0
-    last_frame_offset_right = 0
-    last_frame_offset_composite = 0
-
-    frame_size = 0
-    previous_frame_size = 0
-    channel_type = 0
-    packet_size = 0
-    frame_index = 0
-    upper_limit_feet = 0.0
-    lower_limit_feet = 0.0
-    frequency = 0
-
-    time1 = 0
-    water_depth_feet = 0.0
-    keel_depth_feet = 0.0
-
-    speed_knots = 0.0
-    water_temperature_c = 0.0
-    longitude = 0
-    latitude = 0
-    water_speed_knots = 0.0
-    course_over_ground_radians = 0.0
-    altitude_ft = 0.0
-    heading_radians = 0.0
-
-    validity_flags: ValidityFlag = ValidityFlag(0)
-    time_offset = 0
-    sounded_data = []
-
-    def __init__(self):
-        pass
-
-    def to_bytes(self, frame_size: int):
-        buffer = bytearray(frame_size)
-        struct.pack_into(
-            '<7I<4H<I<2fB',
-            buffer,
-            0,
-            self.frame_offset,
-            self.last_frame_offset_primary,
-            self.last_frame_offset_secondary,
-            self.last_frame_offset_down_scan,
-            self.last_frame_offset_left,
-            self.last_frame_offset_right,
-            self.last_frame_offset_composite,
-            self.frame_size,
-            self.previous_frame_size,
-            self.channel_type,
-            self.packet_size,
-            self.frame_index,
-            self.upper_limit_feet,
-            self.lower_limit_feet,
-            self.frequency,
-        )
-        struct.pack_into(
-            '<Iff', buffer, 60, self.time1, self.water_depth_feet, self.keel_depth_feet
-        )
-        struct.pack_into(
-            '2f2I4fH',
-            buffer,
-            100,
-            self.speed_knots,
-            self.water_temperature_c,
-            self.longitude,
-            self.latitude,
-            self.water_speed_knots,
-            self.course_over_ground_radians,
-            self.altitude_ft,
-            self.heading_radians,
-            int(self.validity_flags),
-        )
-        struct.pack_into('I', buffer, 140, self.time_offset)
-        for i, depth_ft in enumerate(self.sounded_data):
-            if depth_ft < self.lower_limit_feet:
-                b = 0
-            elif depth_ft > self.upper_limit_feet:
-                b = 255
-            else:
-                b = round(
-                    255
-                    * (depth_ft - self.lower_limit_feet)
-                    / (self.upper_limit_feet - self.lower_limit_feet)
-                )
-            buffer[144 + i] = b
-        return buffer
+    # pprint(SL2_HEADER)
+    # pprint(sl_file_header.parse(SL2_HEADER))
+    header = sl_file_header.parse(file_bytes[:8])
+    blocks = []
+    for i in range(8, len(file_bytes), 2064):
+        try:
+            frame = sl2_frame.parse(file_bytes[i:])
+            assert frame.frame_offset == i
+            blocks.append(frame)
+        except c.ConstructError as e:
+            print(f'failed to parse block at {i} for reason: {e}')
+    pass
